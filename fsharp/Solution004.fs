29d0a896064370354265a45f8a8d8e46e0a1a630
Luke Cummings <luke.g.cummings@ca.abb.com>
2015-12-17 12:44:44 -0700

    .gitignore and .gitattributes were missing hence the unstaged commits

---
+++ fsharp/Solution004.fs
@@ -0,0 +1,18 @@
+(*
+    Problem: 4
+
+    Title: Largest palindrome product
+
+    Description:
+        A palindromic number reads the same both ways. The largest palindrome
+        made from the product of two 2-digit numbers is 9009 = 91 Ã— 99.
+        Find the largest palindrome made from the product of two 3-digit
+        numbers.
+
+    Url: https://projecteuler.net/problem=4
+*)
+
+module ProjectEulerSolutions.FSharp.Solution004
+
+let Answer =
+
