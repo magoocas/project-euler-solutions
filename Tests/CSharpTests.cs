6d91ef5b182d9f491a9b80cfc997af3efcbbf32e
Luke Cummings <luke.g.cummings@ca.abb.com>
2015-12-17 12:41:35 -0700

    Solution002 in csharp

--- Tests/CSharpTests.cs
+++ Tests/CSharpTests.cs
@@ -9,5 +9,7 @@ namespace Tests
     {
         [TestMethod]
         public void TestSolution001() => Assert.AreEqual(Solution001.Answer, Answers.Problem001);
+        [TestMethod]
+        public void TestSolution002() => Assert.AreEqual(Solution002.Answer, Answers.Problem002);
     }
 }
