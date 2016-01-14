b0868eabcce8ab93c2654b993ea9e12ce0cd35fe
Luke Cummings <cummings.luke@gmail.com>
2016-01-14 09:11:37 -0700

    Solution 14/15 in c#

---
+++ ProjectEulerSolutions.CSharp/Solution015.cs
@@ -0,0 +1,38 @@
+/*
+    Problem: 15
+
+    Title: Lattice paths
+
+    Description:
+        Starting in the top left corner of a 2x2 grid, and only being able to move to
+        the right and down, there are exactly 6 routes to the bottom right corner.
+        
+            [p015]
+        
+        How many such routes are there through a 20x20 grid?
+        
+
+    Url: https://projecteuler.net/problem=15
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace ProjectEulerSolutions.CSharp
+{
+    public static class Solution015
+    {
+        public static object Answer()
+        {
+            var size = 20;
+            var paths = new long[size + 1];
+            paths[0] = 1;
+            for (int i = 0; i <= size; i++)
+                for (int j = 0; j < size; j++)
+                    paths[j + 1] += paths[j];
+
+            return paths[size];
+        }
+    }
+}
+
