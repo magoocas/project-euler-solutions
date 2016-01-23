74e376035d44d0753840802776f9d956f814865a
Luke Cummings <cummings.luke@gmail.com>
2016-01-23 14:28:27 -0700

    util classes

---
+++ dotnet/ProjectEulerSolutions.CSharp/Utility/Prime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEulerSolutions.CSharp.Utility
+{
+    public static class Prime
+    {
+        public static List<long> GetPrimeFactors(long numberToFactor, long potentialPrime = 2)
+        {
+            var primes = new List<long>();
+            while (potentialPrime * 2 <= numberToFactor)
+            {
+                long remainder;
+                var quotient = Math.DivRem(numberToFactor, potentialPrime, out remainder);
+
+                if (remainder == 0)
+                {
+                    primes.Add(potentialPrime);
+                    numberToFactor = quotient;
+                }
+                else if (potentialPrime == 2)
+                    potentialPrime = 3;
+                else
+                    potentialPrime = potentialPrime + 2;
+            }
+
+            if (numberToFactor > 0)
+                primes.Add(numberToFactor);
+
+            return primes;
+        }
+    }
+}
---
+++ dotnet/ProjectEulerSolutions.CSharp/Utility/Sequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEulerSolutions.CSharp.Utility
+{
+    public static class Sequence
+    {
+        public static IEnumerable<T> Unfold<T>(T seed, Func<T, T> enumerator)
+        {
+            return Unfold(seed, enumerator, x => x);
+        }
+
+        public static IEnumerable<TResult> Unfold<T, TResult>(T seed, Func<T, T> enumerator, Func<T, TResult> selector)
+        {
+            var next = seed;
+
+            while (true)
+            {
+                var res = enumerator(next);
+                if (res == null)
+                    yield break;
+
+                yield return selector(res);
+
+                next = res;
+            }
+        }
+        
+        public static IEnumerable<long> Fibbinoci()
+        {
+            return Unfold(Tuple.Create(0L, 1L), x => Tuple.Create(x.Item2, x.Item1 + x.Item2), x => x.Item1);
+        }
+    }
+}
